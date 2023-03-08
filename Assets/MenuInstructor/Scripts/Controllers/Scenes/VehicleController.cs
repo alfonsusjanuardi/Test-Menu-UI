e0a8f0906d251bc5f451e4f8a44f178ecd2bbbb3
Alfonsus Anugerah Januardi <aanugerahjanuardi@gmail.com>
2023-03-08 13:32:50 +0700

    08/03/2023 13:32 -- Ardi

--- Assets/MenuInstructor/Scripts/Controllers/Scenes/VehicleController.cs
+++ Assets/MenuInstructor/Scripts/Controllers/Scenes/VehicleController.cs
@@ -1,13 +1,57 @@
+using System;
+using System.Data;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+using Meta.Data;
+using Newtonsoft.Json;
 
 public class VehicleController : MonoBehaviour
 {
+    [Header("UI GameObject")]
+    [Space(5)]
+    [SerializeField] private Transform parentTransform;
+    [SerializeField] private GameObject btn;
+
+    [Header("UI")]
+    [Space(5)]
+    [SerializeField] private GameObject canvasPanel;
+    [SerializeField] private TextMeshProUGUI vehicleName;
+
+    [Header("List")]
+    [Space(5)]
+    [SerializeField] private List<VehicleInfo> vehicles = new();
+
+    [TextArea(10,20)]
+    public string json;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vehicles = VehicleInfo.FromJson(json);
+
+        for (int i = 0; i < vehicles.Count; i++)
+        {
+            var copy = Instantiate(btn, parentTransform);
+            var meta = copy.GetComponent<Metadata>();
+            var nama = vehicles[i].name;
+            var desc = vehicles[i].description;
+            var sprite = Resources.Load<Sprite>("Images/" + nama + "/image");
+
+            var vehicle = new VehicleInfo()
+            {
+                name = nama,
+                description = desc,
+                sprite = sprite
+            };
+
+            meta.FindParamComponent<TextMeshProUGUI>("buttonName").text = nama;
+            var button = meta.FindParamComponent<Button>("button");
+            button.onClick.AddListener(delegate { GetInfo(vehicle); });
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +59,23 @@ public class VehicleController : MonoBehaviour
     {
         
     }
+
+    private void GetInfo(VehicleInfo vInfo)
+    {
+        if (canvasPanel.activeSelf == false)
+                canvasPanel.SetActive(true);
+
+        
+    }
+}
+
+[Serializable]
+public class VehicleInfo
+{
+    public string name;
+    public string description;
+    [JsonIgnore] public Sprite sprite;
+
+    public static List<VehicleInfo> FromJson(string json) => JsonConvert.DeserializeObject<List<VehicleInfo>>(json);
+        public static string ToString(List<VehicleInfo> json) => JsonConvert.SerializeObject(json, Formatting.Indented);
 }
