23c8945151f6f2bad3a72ce35439af286f39a960
Alfonsus Anugerah Januardi <aanugerahjanuardi@gmail.com>
2023-03-09 10:39:03 +0700

    09/03/2023 10.37 -- Update Heavy Equipment Menu part 2

--- Assets/MenuInstructor/Scripts/Controllers/Scenes/VehicleController.cs
+++ Assets/MenuInstructor/Scripts/Controllers/Scenes/VehicleController.cs
@@ -11,6 +11,8 @@ using Newtonsoft.Json;
 
 public class VehicleController : MonoBehaviour
 {
+    public static VehicleController instance;
+
     [Header("UI GameObject")]
     [Space(5)]
     [SerializeField] private Transform parentTransform;
@@ -25,12 +27,75 @@ public class VehicleController : MonoBehaviour
     [Header("List")]
     [Space(5)]
     [SerializeField] private List<VehicleInfo> vehicles = new();
+    public List<Toggle> toggles;
+    public Dictionary<string, string> consoles = new();
+
+    [SerializeField] private NetworkController DBConn;
+    private string sql;
 
     [TextArea(10,20)]
     public string json;
 
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
     // Start is called before the first frame update
     void Start()
+    {
+        ConnectionDBVehicle();
+        LoadDataVehicle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ConnectionDBVehicle()
+    {
+        sql = "select * from console_vehicle";
+        IDataReader reader = DBConn.getData(sql);
+
+        while (reader.Read()) {
+            string console_name = (string) reader["console_name"];
+            string vehicle_name = (string) reader["vehicle_name"];
+
+            if (!string.IsNullOrEmpty(console_name) && !string.IsNullOrEmpty(vehicle_name)) {
+                consoles.Add(vehicle_name, console_name);
+            }
+        }
+
+        DBConn.CloseConnection();
+    }
+
+    private void GetInfo(VehicleInfo vInfo)
+    {
+        foreach (Toggle toggle in toggles)
+            toggle.isOn = false;
+
+        if (canvasPanel.activeSelf == false)
+                canvasPanel.SetActive(true);
+
+        vehicleName.text = vInfo.name;
+        vehicleDescription.text = vInfo.description;
+        imageHeavyEquipment.sprite = vInfo.sprite;
+
+        string consoleName = GetConsoleFromVehicle(vInfo.name);
+        Toggle consoleToggle = GetConsoleToggle(consoleName);
+
+        if (consoleToggle != null)
+            consoleToggle.isOn = true;
+        else
+            Debug.Log("No Console Attach To This Vehicle!!");
+    }
+
+    private void LoadDataVehicle()
     {
         vehicles = VehicleInfo.FromJson(json);
 
@@ -55,20 +120,39 @@ public class VehicleController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public Toggle GetConsoleToggle(string consoleName)
     {
-        
+        foreach (Toggle toggle in toggles)
+        {
+            TextMeshProUGUI tmp = toggle.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+            if (tmp.text == consoleName)
+                return toggle;
+        }
+
+        return null;
     }
 
-    private void GetInfo(VehicleInfo vInfo)
+    public List<string> GetVehiclesFromConsole(string vehicleName)
     {
-        if (canvasPanel.activeSelf == false)
-                canvasPanel.SetActive(true);
+        List<string> vehicles = new();
+        foreach (KeyValuePair<string,string> keyValue in consoles)
+        {
+            if (keyValue.Value == vehicleName)
+                vehicles.Add(keyValue.Key);
+        }
+        
+        return vehicles;
+    }
 
-        vehicleName.text = vInfo.name;
-        vehicleDescription.text = vInfo.description;
-        imageHeavyEquipment.sprite = vInfo.sprite;
+    public string GetConsoleFromVehicle(string consoleName)
+    {
+        foreach (KeyValuePair<string,string> keyValue in consoles)
+        {
+            if (keyValue.Key == consoleName)
+                return keyValue.Value;
+        }
+
+        return null;
     }
 }
 
---
+++ Assets/MenuInstructor/Scripts/Variables/Consoles.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+[System.Serializable]
+public class Consoles : MonoBehaviour
+{
+    public string consoles, vehicles;
+}
